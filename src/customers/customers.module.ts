5b538f6de1244b9119c9fc28398079a518b7407f
Hans <hungvu193@users.noreply.github.com>
2022-07-21 16:07:53 +0700

    middleware playground

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
+import { ValidateCustomerMiddleware } from './middleware/validate-customer.middleware';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { CustomersController } from './controllers/customers/customers.controller';
 import { CustomersService } from './services/customers/customers.service';
 
 @Module({
   controllers: [CustomersController],
-  providers: [CustomersService]
+  providers: [CustomersService],
 })
-export class CustomersModule {}
+// TODO: https://docs.nestjs.com/middleware
+export class CustomersModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ValidateCustomerMiddleware)
+      .exclude({
+        path: 'api/customers',
+        method: RequestMethod.GET,
+      })
+      .forRoutes(CustomersController);
+  }
+}
--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,22 +1,33 @@
-import { Controller, Get, HttpException, Inject, Param, HttpStatus, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  Inject,
+  Param,
+  HttpStatus,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+} from '@nestjs/common';
 import { UsersService } from 'src/users/services/users/users.service';
 import { SerializeUser } from 'src/users/types';
 
 @Controller('users')
 export class UsersController {
-  constructor(@Inject('USER_SERVICE') private readonly userServices: UsersService) {
+  constructor(
+    @Inject('USER_SERVICE') private readonly userServices: UsersService,
+  ) {}
 
-  }
-  @Get("")
+  @UseInterceptors(ClassSerializerInterceptor)
+  @Get('')
   getUsers() {
-    return this.userServices.getUsers()
+    return this.userServices.getUsers();
   }
 
   @UseInterceptors(ClassSerializerInterceptor)
-  @Get("/:username")
+  @Get('/:username')
   getUserByName(@Param('username') username: string) {
     const user = this.userServices.getUserByUsername(username);
     if (user) return new SerializeUser(user);
-    else throw new HttpException('User not found', HttpStatus.BAD_REQUEST)
+    else throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
   }
 }
